4cd3e7651ec7d3b43be4a24f9324b1baf14c6c2d
daviifpi <113477731+daviifpi@users.noreply.github.com>
2022-12-27 18:31:32 -0300

    Add files via upload

---
+++ banco.js
@@ -0,0 +1,7 @@
+export default alunos = [
+  {id:0, nome:"Açai No grau", preco:9.0, descricao:"Açai No grau para refrescar seu dia",img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcOrg_c0c49GekUNv1AHrYrrva1iBZGEe3zFyLSpOdZav3dwxcWbVkrJXoXNxoK-ElJ-8&usqp=CAU"},
+  {id:1, nome:"Açai Cremoso", preco:7.0, descricao:"Um açai cremoso para você se deliciar",img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI63sbLWHCDvPDrBVT1jVQ0r5ddrLMhy2yiw&usqp=CAU"},
+  {id:2, nome:"Açai congelado", preco:8.0, descricao:"Um açaí congelado bem refrescante",img:"https://loja.barracadoze.com.br/wp-content/uploads/sites/5/2020/10/Nota2-novosite.jpg"},
+  {id:3, nome:"Açai na casquinha", preco:10.0, descricao:"Açai na casquinha para você se deliciar!",img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3N5S_YtxfLAv3rBfQNkAXLg6ZxU1KoyEsbw&usqp=CAU"}
+] 
+
